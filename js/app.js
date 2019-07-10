e8a4b37ceb1bb44af4779a3b113cd56149139e4e
Quang Nguyen <quangn2595@gmail.com>
2019-07-10 09:53:57 -0700

    display images

--- js/app.js
+++ js/app.js
@@ -11,16 +11,24 @@ function Images(url, title, description, keyword, horns) {
 
 Images.list = [];
 
-const createImageObject = function(data) { 
-  data.forEach(element => {
-    new Images(element.image_url, element.title, element. description, element.keyword, element.horns);
-  });
-}
 
 $.get('./data/page-1.json', (data) => {
   data.forEach(element => {
     new Images(element.image_url, element.title, element. description, element.keyword, element.horns);
   })
 });
-
 console.log(Images.list);
+
+function displayImages() { 
+  Images.list.forEach(image => { 
+    const $newImage = $('.photo-template').clone();
+
+    $newImage.find('h2').text(image.title);
+    $newImage.find('p').text(image.description);
+    $newImage.find('img').attr({ src: image.image_url, alt: image.keyword});
+
+    $('main').append($newImage);
+  });
+}
+
+displayImages();
